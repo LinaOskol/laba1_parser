9adc1ee3ac20e913d3c0b697cad5b90c74819967
Alina <lina.oskolkova.97@mail.ru>
2023-02-23 18:31:31 +0600

    feat: completed parser

--- parser.py
+++ parser.py
@@ -1,12 +1,20 @@
-from bs4 import BeautifulSoup
 import requests
-url='https://www.omgtu.ru/general_information/faculties/'
-page=requests.get(url)
+from bs4 import BeautifulSoup
+
+url = 'https://www.omgtu.ru/general_information/faculties/'
+page = requests.get(url)
 print(page.status_code)
 
-filteredNews = []
-allNews = []
+faculties = []
+allFaculties = []
+file = open('result.txt', 'w')
 soup = BeautifulSoup(page.text, "html.parser")
-print(soup)
-allNews = soup.findAll('', class_='')
-print(allNews)
+# print(soup)
+allFaculties = soup.find_all('div', class_='main__content')
+for facult in allFaculties:
+    if facult.find('li') is not None:
+        faculties.append(facult.text)
+for facult in faculties:
+    file.write(facult)
+
+file.close()
