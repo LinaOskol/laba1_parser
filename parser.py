4eb10307e0b2f2ba83f8c9e91f1aa3fcfe83b58f
LinaOskol <124796450+LinaOskol@users.noreply.github.com>
2023-02-24 17:17:11 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,10 +9,10 @@ faculties = []
 allFaculties = []
 file = open('result.txt', 'w')
 soup = BeautifulSoup(page.text, "html.parser")
-# print(soup)
+
 allFaculties = soup.find_all('div', id='pagecontent')
 for facult in allFaculties:
-    #if facult.find('ul') is not None:
+    
         faculties.append(facult.find('ul').text)
 for facult in faculties:
     file.write(facult)
