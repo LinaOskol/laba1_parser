7dc74457a987eacfdbf7b91b5fec2d943b9c61ef
Alina <lina.oskolkova.97@mail.ru>
2023-04-04 21:38:58 +0600

    chore: add result of parser

--- Parser.py
+++ Parser.py
@@ -3,19 +3,19 @@ from bs4 import BeautifulSoup
 
 
 def parse():
-    url = 'https://www.omgtu.ru/general_information/faculties/'  # адрес сайта
-    page = requests.get(url)  # делаем запрос на сайт
-    print(page.status_code)  # вывод ответа сайта, если 200 то все успешно
+    url = 'https://www.omgtu.ru/general_information/faculties/'
+    page = requests.get(url)
+    print(page.status_code)
     faculties = []
     allFaculties = []
-    file = open('result.txt', 'w')  # открываем файл с параметром записи в него, перед записью файл будет очищен
-    soup = BeautifulSoup(page.text, "html.parser")  # передаем нашу страницу в bs4
-    allFaculties = soup.find_all('div', id='pagecontent')  # ищем контейнер с нужным классом
+    file = open('result.txt', 'w')
+    soup = BeautifulSoup(page.text, "html.parser")
+    allFaculties = soup.find_all('div', id='pagecontent')
 
     for facult in allFaculties:
-        faculties.append(facult.find('ul').text)  # ищем теги ul и вносим в массив
+        faculties.append(facult.find('ul').text)
 
-    faculties = faculties[0][0:len(faculties[0]) - 3].split('\n')  # разделяем по \n нулевой элемент,чтобы получить массив
+    faculties = faculties[0][0:len(faculties[0]) - 3].split('\n')
 
     for facult in faculties:
         if facult != '':
